fa31556e704eaea0958ebcb649516f320cd9cab8
Noam Hadar <nhthebest.noam.hadar@gmail.com>
2018-06-22 09:19:10 +1200

    Add net cat

---
+++ NetWorking/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Net.Sockets;
+
+namespace NEJC_Inc
+{
+    class netcat
+    {
+        private NetworkStream stream;
+        private StreamWriter streamw;
+        private StreamReader streamr;
+        private TcpClient myclient = new TcpClient();
+        public void connect(string host, int port)
+        {
+            myclient.Connect(host, port);
+            stream = myclient.GetStream();
+            streamw = new StreamWriter(stream);
+            streamr = new StreamReader(stream);
+        }
+        public void client_send(string text)
+        {
+            streamw.WriteLine(text);
+            streamw.Flush();
+        }
+        public string client_receive()
+        {
+            return streamr.ReadLine();
+        }
+    }
+
+}
